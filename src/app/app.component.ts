f448c192543ff67f405eec48ba3628b36a01062d
EkaBachrudin <vvalentine825@gmail.com>
2023-09-10 16:36:44 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetUserProfileUseCase } from 'src/domain/usecases/get-user-profile.usecase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(public getUserProfileUseCase: GetUserProfileUseCase) {}
   title = 'wedding-frontend';
+
+  ngOnInit() {
+    this.getUserProfileUseCase.execute().subscribe({});
+  }
 }
